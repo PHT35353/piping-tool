d1b98542e9168ee84b9d5d78993937710660addb
PHT373 <168071323+PHT35353@users.noreply.github.com>
2024-10-07 21:07:21 +0200

    Update piping.py

--- piping.py
+++ piping.py
@@ -201,7 +201,7 @@ mapbox_map_html = f"""
                     }});
 
                     let distanceUnit = length >= 1 ? 'km' : 'm';
-                    let {distanceValue} = length >= 1 ? length.toFixed(2) : (length * 1000).toFixed(2);
+                    let distanceValue = length >= 1 ? length.toFixed(2) : (length * 1000).toFixed(2);
                     
 
                     sidebarContent += '<p>Line ' + featureNames[feature.id] + ' belongs to ' + (startLandmark?.properties.name || 'Unknown') + ' - ' + (endLandmark?.properties.name || 'Unknown') + ': ' + distanceValue + ' ' + distanceUnit + '</p>';
@@ -542,7 +542,7 @@ def pipe_main():
             Pipe_Material = choose_pipe_material(P, T, M)
             st.write(f"Selected Pipe Material: {Pipe_Material}")
             Pipe_finder(Pipe_Material, P, distanceValue)
-            distanceValue = st.number_input("Enter the distance value (in meters):", min_value=0.0, format="%.2f")
+           
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
